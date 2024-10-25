50c3ce379f34df600e1a24bd4758b22a702ad28c
양정화 <siltarre@gmail.com>
2024-10-26 02:19:18 +0900

    🐛 빌드 에러
    포멧터 미적용으로 에러 발생하여 해결

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,111 +1,73 @@
+// .eslintrc.js
 module.exports = {
-  // 코드 실행 환경 지정 (예: 브라우저, Node.js 등)
   env: {
-    browser: true, // 브라우저에서 실행될 코드
-    node: true, // Node.js에서 실행될 코드
-    es2021: true, // ECMAScript 2021 문법 사용
+    browser: true,
+    node: true,
+    es2021: true,
   },
 
-  // 자바스크립트의 구문 분석기 설정
+  parser: '@babel/eslint-parser', // JSX와 최신 문법을 파싱하기 위해 babel 파서를 사용
   parserOptions: {
-    ecmaVersion: 12, // ECMAScript 버전 12 (ES2021) 지원
-    sourceType: "module", // ES 모듈 시스템 사용
+    ecmaVersion: 12,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true, // JSX를 사용할 수 있도록 설정
+    },
+    requireConfigFile: false, // 별도의 Babel 설정 파일을 요구하지 않도록 설정
+    babelOptions: {
+      plugins: ['@babel/plugin-syntax-jsx'], // JSX 구문을 인식하도록 플러그인 추가
+    },
   },
 
-  // 기본적으로 사용할 ESLint 규칙의 확장 설정
-  extends: [
-    "eslint:recommended", // ESLint에서 권장하는 기본 규칙 사용
-    "prettier", // Prettier와의 충돌 방지를 위해 Prettier 규칙 추가
-  ],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
 
-  // 사용할 플러그인 목록 (기본적으로 ESLint에 없는 추가 규칙 모음)
-  plugins: [
-    "prettier", // Prettier 플러그인 추가
-    "react", // React 관련 플러그인 추가
-    "import", // import 관련 플러그인 추가
-    "styled-components-a11y", // styled-components 관련 플러그인 추가
-    "react-hooks", // React Hooks 관련 플러그인 추가
-  ],
+  plugins: ['prettier', 'react', 'import', 'react-hooks'],
 
-  // 코드 규칙 정의
   rules: {
-    "no-unused-vars": "warn", // 사용되지 않는 변수가 있을 때 경고 표시
-    "no-console": "off", // console.log 사용을 허용
-    eqeqeq: "error", // 일치 연산자(===) 사용을 강제
-    indent: ["error", 2], // 들여쓰기를 스페이스 2칸으로 설정
-    quotes: ["error", "single"], // 문자열에 작은따옴표 사용 강제
-    semi: ["error", "always"], // 명령문 끝에 세미콜론 사용 강제
-    curly: ["error", "all"], // 블록 구문에서 중괄호 생략 금지 및 줄바꿈 강제
-    "arrow-body-style": ["error", "as-needed"], // 화살표 함수에서 바로 결과 반환 시 중괄호와 return 생략
-    "prettier/prettier": [
-      "error",
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    eqeqeq: 'error',
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    curly: ['error', 'all'],
+    'arrow-body-style': ['error', 'as-needed'],
+    'prettier/prettier': [
+      'error',
       {
-        singleQuote: true, // 문자열에 작은따옴표 사용
-        semi: true, // 명령문 끝에 세미콜론 사용
-        useTabs: false, // 탭 대신 공백 사용
-        tabWidth: 2, // 들여쓰기 공백 수 2칸
-        trailingComma: "all", // 가능한 경우 후행 쉼표 사용
-        printWidth: 80, // 줄 바꿈할 길이 80자
-        bracketSpacing: true, // 객체 리터럴에서 괄호 사이에 공백 추가
-        arrowParens: "always", // 화살표 함수의 매개변수에 항상 괄호 사용
-        endOfLine: "auto", // 운영 체제에 따라 줄 끝 처리 방식 사용
+        singleQuote: true,
+        semi: true,
+        useTabs: false,
+        tabWidth: 2,
+        trailingComma: 'all',
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: 'always',
+        endOfLine: 'lf',
       },
     ],
-    "func-style": ["error", "declaration", { allowArrowFunctions: true }], // 컴포넌트형 함수는 function 선언문 사용, 일반 함수는 화살표 함수 사용
-    "react/function-component-definition": [
-      "error",
+    'func-style': ['error', 'declaration', { allowArrowFunctions: true }],
+    'react/function-component-definition': [
+      'error',
       {
-        namedComponents: "function-declaration", // 컴포넌트형 함수는 function 선언문으로 작성
-        unnamedComponents: "arrow-function", // 익명 컴포넌트는 화살표 함수로 작성
+        namedComponents: 'function-declaration',
+        unnamedComponents: 'arrow-function',
       },
     ],
-    "import/order": [
-      "error",
+    'import/order': [
+      'error',
       {
         groups: [
-          "builtin",
-          "external",
-          ["index", "sibling", "parent", "internal"],
-          ["react"], // React 라이브러리 최상단
-          ["external", "api"], // API 관련
-          ["external", "style"], // 스타일 컴포넌트 관련
-          ["external", "image"], // 이미지 관련
-          ["internal", "component"], // 컴포넌트 관련
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling', 'index'],
         ],
-        "newlines-between": "always",
-        alphabetize: { order: "asc", caseInsensitive: true }, // 알파벳 순으로 정렬
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
       },
     ],
-    "styled-components-a11y/rule-name": [
-      "error",
-      // css 속성 순서 정렬
-      {
-        propertiesOrder: [
-          "display",
-          "position",
-          "top",
-          "right",
-          "bottom",
-          "left",
-          "z-index",
-          "flex",
-          "grid",
-          "align-items",
-          "justify-content",
-          "width",
-          "height",
-          "margin",
-          "padding",
-          "border",
-          "background",
-          "color",
-          "font",
-          "animation",
-          "transition",
-        ],
-      },
-    ],
-    "react-hooks/rules-of-hooks": "error", // Hooks는 컴포넌트의 최상단에서 호출해야 함
-    "react-hooks/exhaustive-deps": "warn", // effect 의존성 배열 검사
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 };
