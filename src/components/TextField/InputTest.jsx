7c85623c12f89e7e258f2303f2e76608d8901943
정평안 <qwerjpa7@jeongpyeong-an-ui-noteubug.local>
2024-10-28 03:21:59 +0900

    ⚡️ 스크롤바 스타일 조정
    스크롤바 스타일 조정 히히

--- src/components/TextField/InputTest.jsx
+++ src/components/TextField/InputTest.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 import Input from './Input';
+import Textarea from './Textarea';
 
 function InputTest() {
   const [value, setValue] = useState('');
@@ -26,6 +27,7 @@ function InputTest() {
         error={error}
         errMessage={'내용을 입력해주세요'}
       />
+      <Textarea />
     </>
   );
 }
---
+++ src/components/TextField/Textarea.jsx
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+
+const StyledTextarea = styled.textarea`
+  width: 520px;
+  height: 100px;
+
+  border: none;
+
+  resize: none;
+
+  ${({ theme }) => theme.fontTheme['18Regular']}
+
+  //스크롤바 스타일
+  &::-webkit-scrollbar {
+    //스크롤바 너비
+    width: 4px;
+  }
+
+  &::-webkit-scrollbar-track {
+    //스크롤바 배경
+    border-radius: 8px;
+    background: #fff;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    //스크롤바 색상
+    background: #ccc;
+    border-radius: 8px;
+  }
+
+  &::-webkit-scrollbar-thumb:hover {
+    background: #555;
+  }
+`;
+
+function Textarea() {
+  return (
+    <StyledTextarea>
+      코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 하세요!
+      코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또
+      하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또
+      하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또
+      하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또
+      하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또
+      하세요!
+    </StyledTextarea>
+  );
+}
+
+export default Textarea;
