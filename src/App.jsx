423f3cece637f6c5453c294855717c43a6b2fb1b
정평안 <qwerjpa7@jeongpyeong-an-ui-noteubug.local>
2024-10-27 20:19:58 +0900

    💄 Noto Sans KR(한글 Ronoto)로 reset.css 바꾸기
    한글 Roboto인 Noto Sans KR import 및 적용

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import './styles/normalize.css';
 import './styles/reset.css';
 
 function App() {
-  return <></>;
+  return (
+    <div>
+      코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 하세요!
+    </div>
+  );
 }
 
 export default App;
