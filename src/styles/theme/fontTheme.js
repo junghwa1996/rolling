46397c914e5eb798370f8fd8ca8c7702ef74124f
정평안 <qwerjpa7@jeongpyeong-an-ui-noteubug.local>
2024-10-27 20:53:34 +0900

    💄 Pretendard로 다시 바꿈 & theme 폴더 생성
    - 원본 Figma로 다시 확인해보니 pretendard로 되어 있어 (옮기는 과정 중 변환,, ) 다시 설정 고쳐줌 -> 모두가 원본 Figma 보고 작업하세요

---
+++ src/styles/theme/fontTheme.js
@@ -0,0 +1,30 @@
+const fontTheme = {
+  // ${({ theme }) => theme.fontTheme["28Bold"]}
+
+  "28Bold": {
+		fontWeight: "700",  //Bold
+		fontSize: "28px",
+		lineHeight: "42px"
+	},
+
+  "24Bold": {
+		fontWeight: "700",  //Bold
+		fontSize: "24px",
+		lineHeight: "36px"
+	},
+	
+	"24Regular": {
+		fontWeight: "400",  //Regular
+		fontSize: "24px",
+		lineHeight: "36px"
+	},
+	
+	"24Regular": {
+		fontWeight: "400",  //Regular
+		fontSize: "24px",
+		lineHeight: "36px"
+	},
+	
+
+
+}
