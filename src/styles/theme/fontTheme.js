f71b42c606e075c98c8a685a7b8bcc458107f484
정평안 <qwerjpa7@jeongpyeong-an-ui-noteubug.local>
2024-10-27 21:24:22 +0900

    💄 Theme 관련 파일 추가
    font, color theme 관련 파일 추가

--- src/styles/theme/fontTheme.js
+++ src/styles/theme/fontTheme.js
@@ -1,30 +1,103 @@
 const fontTheme = {
   // ${({ theme }) => theme.fontTheme["28Bold"]}
 
-  "28Bold": {
-		fontWeight: "700",  //Bold
-		fontSize: "28px",
-		lineHeight: "42px"
-	},
+  '28Bold': {
+    fontWeight: '700',
+    fontSize: '28px',
+    lineHeight: '42px',
+    letterSpacing: '-1%',
+  },
 
-  "24Bold": {
-		fontWeight: "700",  //Bold
-		fontSize: "24px",
-		lineHeight: "36px"
-	},
-	
-	"24Regular": {
-		fontWeight: "400",  //Regular
-		fontSize: "24px",
-		lineHeight: "36px"
-	},
-	
-	"24Regular": {
-		fontWeight: "400",  //Regular
-		fontSize: "24px",
-		lineHeight: "36px"
-	},
-	
+  '24Bold': {
+    fontWeight: '700',
+    fontSize: '24px',
+    lineHeight: '36px',
+    letterSpacing: '-1%',
+  },
+
+  '24Regular': {
+    fontWeight: '400',
+    fontSize: '24px',
+    lineHeight: '36px',
+  },
+
+  '20Bold': {
+    fontWeight: '700',
+    fontSize: '20px',
+    lineHeight: '30px',
+    letterSpacing: '-1%',
+  },
+
+  '20Regular': {
+    fontWeight: '400',
+    fontSize: '20px',
+    lineHeight: '30px',
+    letterSpacing: '-1%',
+  },
+
+  '18Bold': {
+    fontWeight: '700',
+    fontSize: '18px',
+    lineHeight: '28px',
+    letterSpacing: '-1%',
+  },
+
+  '18Regular': {
+    fontWeight: '400',
+    fontSize: '18px',
+    lineHeight: '28px',
+    letterSpacing: '-1%',
+  },
+
+  '16Bold': {
+    fontWeight: '700',
+    fontSize: '16px',
+    lineHeight: '26px',
+    letterSpacing: '-1%',
+  },
+
+  '16Regular': {
+    fontWeight: '400',
+    fontSize: '16px',
+    lineHeight: '26px',
+    letterSpacing: '-1%',
+  },
+
+  '15Bold': {
+    fontWeight: '700',
+    fontSize: '15px',
+    lineHeight: '22px',
+    letterSpacing: '-1%',
+  },
+
+  '15Regular': {
+    fontWeight: '400',
+    fontSize: '15px',
+    lineHeight: '22px',
+    letterSpacing: '-1%',
+  },
+
+  '14Bold': {
+    fontWeight: '700',
+    fontSize: '14px',
+    lineHeight: '20px',
+    letterSpacing: '-0.5%',
+  },
+
+  '14Regular': {
+    fontWeight: '400',
+    fontSize: '14px',
+    lineHeight: '20px',
+    letterSpacing: '-0.5%',
+  },
+
+  '12Regular': {
+    fontWeight: '400',
+    fontSize: '12px',
+    lineHeight: '18px',
+    letterSpacing: '-0.5%',
+  },
+};
 
 
-}
+export default fontTheme;
