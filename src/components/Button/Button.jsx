2b4d75b68fed309f0049ffb67c9614b1d0609c39
swp91 <seongwoo4477@gmail.com>
2024-10-28 16:09:05 +0900

    프롭 디폴트값 구조분해할당식으로 변경

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -4,7 +4,25 @@ import { Link } from 'react-router-dom';
 
 import { StButton } from './Button.styles';
 
-function Button({ size, color, onClick, children, disabled, type, to }) {
+Button.propTypes = {
+  size: PropTypes.oneOf(['s', 'm', 'l', 'xl']),
+  color: PropTypes.oneOf(['primary', 'secondary']),
+  onClick: PropTypes.func,
+  disabled: PropTypes.bool,
+  children: PropTypes.node.isRequired,
+  type: PropTypes.oneOf(['button', 'submit', 'reset']),
+  to: PropTypes.string,
+};
+
+function Button({
+  size = 's',
+  color = 'primary',
+  onClick = () => {},
+  children,
+  disabled = false,
+  type = 'button',
+  to,
+}) {
   const TAG = to ? Link : 'button';
   return (
     <StButton
@@ -20,23 +38,5 @@ function Button({ size, color, onClick, children, disabled, type, to }) {
     </StButton>
   );
 }
-
-Button.propTypes = {
-  size: PropTypes.oneOf(['s', 'm', 'l', 'xl']),
-  color: PropTypes.oneOf(['primary', 'secondary']),
-  onClick: PropTypes.func,
-  disabled: PropTypes.bool,
-  children: PropTypes.node.isRequired,
-  type: PropTypes.oneOf(['button', 'submit', 'reset']),
-  to: PropTypes.string,
-};
-
-Button.defaultProps = {
-  size: 's',
-  color: 'primary',
-  onClick: () => {},
-  disabled: false,
-  type: 'button',
-};
 
 export default Button;
