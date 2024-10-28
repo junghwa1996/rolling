a32afdf35f07d79a52a935934f67bd0b339820fb
swp91 <seongwoo4477@gmail.com>
2024-10-28 21:24:13 +0900

    상수 네이밍규칙에 맞게 수정

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { LogoArea } from './Logo.styles';
-import LogoImg from '../../assets/RollingLogo.svg';
+import LOGO_IMG from '../../assets/RollingLogo.svg';
 
 function Logo() {
   return (
     <LogoArea>
       <Link to="/">
-        <img src={LogoImg} alt="롤링 로고" />
+        <img src={LOGO_IMG} alt="롤링 로고" />
       </Link>
     </LogoArea>
   );
