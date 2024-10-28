08c8e85399c7880adb986e55e923b3e4844fa9f4
swp91 <seongwoo4477@gmail.com>
2024-10-28 21:50:07 +0900

    ✨  공통컴포넌트 - Profile 작업

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,22 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
-function Profile() {
-  return <div>Profile</div>;
+import { ProfileArea, ProfileImg } from './Profile.styles';
+import DEFAULT_IMAGE from '../../assets/default_profile.svg';
+
+Profile.propTypes = {
+  imageURL: PropTypes.string,
+  size: PropTypes.oneOf(['s', 'm']),
+};
+
+function Profile({ imageURL, size = 's' }) {
+  const IMG_URL = imageURL || DEFAULT_IMAGE;
+
+  return (
+    <ProfileArea size={size}>
+      <ProfileImg src={IMG_URL} alt="프로필" />
+    </ProfileArea>
+  );
 }
 
 export default Profile;
