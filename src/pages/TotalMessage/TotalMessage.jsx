8da7159a72381d8e7976f1b273ceba3426bf7068
Hwichul Shin <shinhwiiron@gmail.com>
2024-10-28 16:19:59 +0900

    🎨 데이터를 직접 호출하는 것이 아닌 리스트에서 prop으로 전달
    기존 : 메시지 데이터를 직접 호출하여 리스트 페이지에서 id 값만 전달받아서 메시지 남긴 사람을 노출
    
    변경 : 직접적으로 데이터를 호출하는 것이 아닌 리스트 페이지에서 해당 롤링페이퍼의 메시지 데이터를 prop으로 전달받는 형식으로 변경

--- src/pages/TotalMessage/TotalMessage.jsx
+++ src/pages/TotalMessage/TotalMessage.jsx
@@ -1,7 +1,11 @@
+// NOTE 아바타 기능 (메시지 남긴 사람 수)
+// recentMessages - 해당 롤링페이퍼의 메시지(최대 3개) 데이터를 배열로
+// messageCount - 해당 롤링페이퍼의 메시지의 총 갯수
+// direction - 가로, 세로형에 따라 row, column
+// 위 3가지 prop으로 전달해 주시면 됩니다.
 import Avatar from '@mui/joy/Avatar';
 import AvatarGroup from '@mui/joy/AvatarGroup';
 import PropTypes from 'prop-types';
-import { useEffect, useState } from 'react';
 
 import {
   AvatarGroupStyle,
@@ -9,58 +13,44 @@ import {
   StyledMessageCount,
   StyledTotalMessage,
 } from './TotalMessage.styles';
-import { getMessagesList } from '../../service/api';
 
-// <TotalMessage rollingItemId={rollingItem.id} direction={'row'} />
-// TotalMessage 컴포넌트에 해당하는 롤링페이퍼의 id와 방향 설정만 prop으로 전달해주시면 됩니다.
-function TotalMessage({ rollingItemId, direction }) {
-  const [messageList, setMessageList] = useState([]);
-
-  const maxAvatars = 3; // 표시할 최대 아바타 수
-  const surplus = messageList.length - maxAvatars; // 초과 아바타 수
-
-  useEffect(() => {
-    const handleMessageListLoad = async () => {
-      try {
-        const res = await getMessagesList(rollingItemId);
-        const { results } = res;
-        setMessageList(results);
-      } catch (error) {
-        console.error(
-          '메시지 리스트를 불러오는데 오류가 발생 했습니다.:',
-          error,
-        );
-      }
-    };
-
-    handleMessageListLoad();
-  }, []);
-
+function TotalMessage({
+  recentMessages = [],
+  messageCount = 0,
+  direction = 'row',
+}) {
   return (
-    <StyledTotalMessage direction={direction}>
-      <AvatarGroup sx={AvatarGroupStyle}>
-        {messageList.slice(0, maxAvatars).map((avatar, index) => (
-          <Avatar
-            key={index}
-            src={avatar.profileImageURL}
-            alt={`Avatar ${index + 1}`}
-          />
-        ))}
+    <>
+      {/* message가 있을 경우에만 노출됩니다. */}
+      {messageCount !== 0 && (
+        <StyledTotalMessage direction={direction}>
+          <AvatarGroup sx={AvatarGroupStyle}>
+            {recentMessages.map((avatar, index) => (
+              <Avatar
+                key={index}
+                src={avatar.profileImageURL}
+                alt={`Avatar ${index + 1}`}
+              />
+            ))}
 
-        {/* 초과 아바타 수 표시 */}
-        {surplus > 0 && <Avatar sx={avatarStyle(direction)}>+{surplus}</Avatar>}
-      </AvatarGroup>
+            {/* 초과 아바타 수 표시 */}
+            {messageCount > 3 && (
+              <Avatar sx={avatarStyle(direction)}>+{messageCount - 3}</Avatar>
+            )}
+          </AvatarGroup>
 
-      <StyledMessageCount direction={direction}>
-        <span>{messageList.length}</span>명이 작성했어요!
-      </StyledMessageCount>
-    </StyledTotalMessage>
+          <StyledMessageCount direction={direction}>
+            <span>{messageCount}</span>명이 작성했어요!
+          </StyledMessageCount>
+        </StyledTotalMessage>
+      )}
+    </>
   );
 }
 
-// PropTypes로 rollingItemId의 타입을 string 또는 number로 지정 (예시)
 TotalMessage.propTypes = {
-  rollingItemId: PropTypes.number,
+  recentMessages: PropTypes.array,
+  messageCount: PropTypes.number,
   direction: PropTypes.oneOf(['row', 'column']),
 };
 
