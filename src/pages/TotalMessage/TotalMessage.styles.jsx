378bc63f9837bf0a8c6784fedca9afe8a7c1d4a2
Hwichul Shin <shinhwiiron@gmail.com>
2024-10-28 16:43:38 +0900

    💄 px -> rem 변경

--- src/pages/TotalMessage/TotalMessage.styles.jsx
+++ src/pages/TotalMessage/TotalMessage.styles.jsx
@@ -4,27 +4,27 @@ const directionStyles = {
   row: {
     alignItems: 'center',
     flexDirection: 'row',
-    margin: '0 0 0 11px',
-    border: '1px solid #E3E3E3',
+    margin: '0 0 0 1.1rem',
+    border: '0.1rem solid #E3E3E3',
   },
   column: {
     alignItems: 'flex-start',
     flexDirection: 'column',
-    margin: '12px 0 0 0',
+    margin: '1.2rem 0 0 0',
     border: 'none',
   },
 };
 
 const AvatarGroupStyle = {
-  '--Avatar-size': '28px',
-  '--Avatar-ringSize': '1px',
+  '--Avatar-size': '2.8rem',
+  '--Avatar-ringSize': '0.1rem',
 };
 
 const avatarStyle = (direction) => ({
   border: directionStyles[direction].border,
   boxShadow: 'none',
   background: '#fff',
-  fontSize: '12px',
+  fontSize: '1.2rem',
   fontWeight: '500',
   color: '#484848',
 });
