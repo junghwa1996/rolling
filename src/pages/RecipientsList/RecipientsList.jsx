be444875b543f1437db814186be4bc3082f65692
양정화 <siltarre@gmail.com>
2024-10-29 03:56:13 +0900

    🚧 롤링리스트 구현은 완료했으나 서로 다른 요청 보내게 해야함
    - 첫번째 swiper는 인기순
    - 두번째 swiper는 최신순

--- src/pages/RecipientsList/RecipientsList.jsx
+++ src/pages/RecipientsList/RecipientsList.jsx
@@ -1,5 +1,4 @@
-import { useRef, useState } from 'react';
-import { styled } from 'styled-components';
+import React, { useRef, useState } from 'react';
 import { Controller, Navigation } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
@@ -10,18 +9,14 @@ import ArrowButton from '../../components/ArrowButton/ArrowButton';
 import useFetchData from '../../hooks/useFetchData';
 import { getRollingList } from '../../service/api';
 
-const Container = styled.div`
-  max-width: 120rem;
-  margin: 0 auto;
-`;
-
-function RecipientsList() {
+function SliderComponent() {
   const { data: rollingListData } = useFetchData(getRollingList, []);
   const rollingList = rollingListData.results;
 
   const swiperRef = useRef(null);
   const [isPrev, setIsPrev] = useState(true);
   const [isNext, setIsNext] = useState(false);
+
   const [controlledSwiper, setControlledSwiper] = useState(null);
 
   const handleSlideChange = (swiper) => {
@@ -30,100 +25,56 @@ function RecipientsList() {
   };
 
   return (
-    <Container>
-      <Swiper
-        modules={[Navigation, Controller]}
-        controller={{ control: controlledSwiper }}
-        navigation={{
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        }}
-        onInit={(swiper) => {
-          swiperRef.current = swiper;
-        }}
-        spaceBetween={20}
-        slidesPerView={4}
-        onSlideChange={handleSlideChange}
-        onSwiper={(swiper) => console.log(swiper)}
-      >
-        {!isPrev && (
-          <ArrowButton
-            className="swiper-button-next"
-            direction="left"
-            onClick={() => swiperRef.current?.slidePrev()}
-          />
-        )}
-        {rollingList.map((item) => (
-          <SwiperSlide key={item.id}>
-            <RecipientCard
-              id={item.id}
-              name={item.name}
-              bgColor={item.backgroundColor}
-              bgImage={item.backgroundImageURL}
-              totalMessage={{
-                recentMessages: item.recentMessages,
-                messageCount: item.messageCount,
-                direction: 'column',
-              }}
-              emojiList={item.topReactions}
-            />
-          </SwiperSlide>
-        ))}
-        {!isNext && (
-          <ArrowButton
-            className="swiper-button-prev"
-            onClick={() => swiperRef.current?.slideNext()}
-          />
-        )}
-      </Swiper>
+    <Swiper
+      spaceBetween={20}
+      slidesPerView={4}
+      modules={[Navigation, Controller]}
+      onSlideChange={handleSlideChange}
+      controller={{ control: controlledSwiper }}
+      onInit={(swiper) => (swiperRef.current = swiper)}
+      onSwiper={(swiper) => {
+        swiperRef.current = swiper;
+        setControlledSwiper(swiper);
+      }}
+      navigation={{
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      }}
+    >
+      {!isPrev && (
+        <ArrowButton
+          direction="left"
+          className="swiper-button-next"
+          onClick={() => swiperRef.current?.slidePrev()}
+        />
+      )}
 
-      <Swiper
-        modules={[Navigation, Controller]}
-        controller={{ control: controlledSwiper }}
-        navigation={{
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        }}
-        onInit={(swiper) => {
-          swiperRef.current = swiper;
-        }}
-        spaceBetween={20}
-        slidesPerView={4}
-        onSlideChange={handleSlideChange}
-        onSwiper={setControlledSwiper}
-      >
-        {!isPrev && (
-          <ArrowButton
-            className="swiper-button-next"
-            direction="left"
-            onClick={() => swiperRef.current?.slidePrev()}
+      {rollingList.map((item) => (
+        <SwiperSlide key={item.id}>
+          <RecipientCard
+            key={item.id}
+            id={item.id}
+            name={item.name}
+            bgColor={item.backgroundColor}
+            bgImage={item.backgroundImageURL}
+            totalMessage={{
+              recentMessages: item.recentMessages,
+              messageCount: item.messageCount,
+              direction: 'column',
+            }}
+            emojiList={item.topReactions}
           />
-        )}
-        {rollingList.map((item) => (
-          <SwiperSlide key={item.id}>
-            <RecipientCard
-              id={item.id}
-              name={item.name}
-              bgColor={item.backgroundColor}
-              bgImage={item.backgroundImageURL}
-              totalMessage={{
-                recentMessages: item.recentMessages,
-                messageCount: item.messageCount,
-                direction: 'column',
-              }}
-              emojiList={item.topReactions}
-            />
-          </SwiperSlide>
-        ))}
-        {!isNext && (
-          <ArrowButton
-            className="swiper-button-prev"
-            onClick={() => swiperRef.current?.slideNext()}
-          />
-        )}
-      </Swiper>
-    </Container>
+        </SwiperSlide>
+      ))}
+
+      {!isNext && (
+        <ArrowButton
+          className="swiper-button-prev"
+          onClick={() => swiperRef.current?.slideNext()}
+        />
+      )}
+    </Swiper>
   );
 }
 
-export default RecipientsList;
+export default SliderComponent;
--- src/pages/RecipientsList/RecipientsListPage.jsx
+++ src/pages/RecipientsList/RecipientsListPage.jsx
@@ -1,9 +1,19 @@
+import { styled } from 'styled-components';
+
 import RecipientsList from './RecipientsList';
+
+const Container = styled.div`
+  max-width: 120rem;
+  margin: 0 auto;
+`;
 
 function RecipientsListPage() {
   return (
     <>
-      <RecipientsList />
+      <Container>
+        <RecipientsList />
+        <RecipientsList />
+      </Container>
     </>
   );
 }
