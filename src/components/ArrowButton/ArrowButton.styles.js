be0811fa30abd8975cd97cebe698a7192453ce99
swp91 <seongwoo4477@gmail.com>
2024-10-28 20:05:28 +0900

    figma 확인후 box-shadow제거

--- src/components/ArrowButton/ArrowButton.styles.js
+++ src/components/ArrowButton/ArrowButton.styles.js
@@ -6,7 +6,6 @@ export const ArrowArea = styled.button`
   border-radius: 50%;
   background-color: white;
   border: 1px solid ${({ theme }) => theme.colorTheme.grayscale[200]};
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
   display: flex;
   align-items: center;
   justify-content: center;
