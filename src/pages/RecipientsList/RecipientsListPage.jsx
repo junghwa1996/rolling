918a6e05985935fefde8bcea63444af71131bc14
양정화 <siltarre@gmail.com>
2024-10-28 22:52:23 +0900

    🧪 카드 UI 테스트
    테스트를 위해 미리 만들었습니당
    해당 로직은 RecipientsList.jsx로 옮겨서 만들어질 예정이에용

--- src/pages/RecipientsList/RecipientsListPage.jsx
+++ src/pages/RecipientsList/RecipientsListPage.jsx
@@ -1,10 +1,30 @@
 import RecipientCard from './RecipientsCard';
+import useFetchData from '../../hooks/useFetchData';
+import { getRollingList } from '../../service/api';
 
 function RecipientsList() {
+  const { data: rollingListData } = useFetchData(getRollingList, []);
+  const rollingList = rollingListData.results;
+
   return (
-    <>
-      <RecipientCard bgColor="blue" />
-    </>
+    <ul>
+      {rollingList.map((item) => (
+        <li key={item.id}>
+          <RecipientCard
+            id={item.id}
+            name={item.name}
+            bgColor={item.backgroundColor}
+            bgImage={item.backgroundImageURL}
+            totalMessage={{
+              recentMessages: item.recentMessages,
+              messageCount: item.messageCount,
+              direction: 'column',
+            }}
+            emojiList={item.topReactions}
+          />
+        </li>
+      ))}
+    </ul>
   );
 }
 
