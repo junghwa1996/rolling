4c6a3b7b48fcca91c5bcfd3220869fdd408f4ed6
정평안 <qwerjpa7@jeongpyeong-an-ui-noteubug.local>
2024-10-29 11:51:08 +0900

    ⚡️ Modal 컴포넌트 구현 및 테스트 완료
    Modal 컴포넌트 구현 및 테스트 완료

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,13 @@
+//import Modal from '../../components/Modal/ModalContainer.jsx';
+import ModalTest from '../../components/Modal/ModalTest';
+
 function HomePage() {
-  return <p>메인 페이지</p>;
+  return (
+    <>
+      <p>메인 페이지</p>
+      <ModalTest />
+    </>
+  );
 }
 
 export default HomePage;
