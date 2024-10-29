f426e8a6fc7911c4728242e069fa7fcadd2f8390
양정화 <siltarre@gmail.com>
2024-10-29 19:12:57 +0900

    💄 createdAt 컬러 수정

--- src/pages/MessagesList/CreatedAt.styles.js
+++ src/pages/MessagesList/CreatedAt.styles.js
@@ -2,5 +2,5 @@ import styled from 'styled-components';
 
 export const CreatedAt = styled.span`
   ${({ theme }) => theme.fontTheme['14Regular']};
-  color: ${({ theme }) => theme.colorTheme.grayscale[500]};
+  color: ${({ theme }) => theme.colorTheme.grayscale[400]};
 `;
