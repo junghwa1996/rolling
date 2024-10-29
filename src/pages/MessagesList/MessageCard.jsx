7082f09b4030a845393b754cd2d719f24adbf428
양정화 <siltarre@gmail.com>
2024-10-30 03:57:46 +0900

    📝 메세지 카드 컴포넌트 문서화

--- src/pages/MessagesList/MessageCard.jsx
+++ src/pages/MessagesList/MessageCard.jsx
@@ -1,3 +1,21 @@
+/**
+ * 메시지 카드 단일 컴포넌트
+ *
+ * @param {Object} props - 컴포넌트에 전달되는 props
+ * @param {string} props.type - 메시지 카드의 타입 (예: 'card', 'modal', 'edit')(필수)
+ * @param {Object} props.MessageData - 메시지 데이터 객체(필수)
+ * @param {string} props.MessageData.name - 보낸 사람의 이름
+ * @param {string} props.MessageData.badgeValue - 라벨 값 (친구, 가족, 동료, 지인 중 하나)
+ * @param {string} props.MessageData.createdAt - 메시지 생성일 (날짜 문자열)
+ * @param {string} props.MessageData.imageUrl - 보낸 사람의 프로필 이미지 URL
+ * @param {string} props.MessageData.text - 메시지 내용
+ * @param {Object} props.onEvent - 이벤트 핸들러 객체
+ * @param {Function} props.onEvent.Modal - 모달을 여는 이벤트 핸들러
+ * @param {Function} props.onEvent.ButtonDelete - 삭제 버튼 클릭 핸들러
+ * @param {Function} props.onEvent.ButtonBtnEdit - 수정 버튼 클릭 핸들러
+ * @returns {JSX.Element} 메시지 카드 컴포넌트
+ */
+
 import PropTypes from 'prop-types';
 
 import {
@@ -20,7 +38,7 @@ MessageCard.propTypes = {
     createdAt: PropTypes.string, // 헤더 생성일
     imageUrl: PropTypes.string, // 유저 이미지
     text: PropTypes.string,
-  }),
+  }).isRequired,
 
   onEvent: PropTypes.shape({
     Modal: PropTypes.func,
