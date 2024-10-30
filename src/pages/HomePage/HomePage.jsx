16b812318e68ff77d7f96545ee5ddd097218812b
양정화 <siltarre@gmail.com>
2024-10-30 11:05:36 +0900

    🔨 포멧 설정대로 수정 및 포멧터양식에 맞게 설정

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,9 @@
-// src/pages/Homepage/Homepage.jsx
-import React from 'react';
-import ToastComponent from '../../components/toast/ToastComponent';
-
-const Homepage = () => {
+function Homepage() {
   return (
     <div>
       <h1>홈페이지</h1>
-      <ToastComponent /> {/* ToastComponent 추가 */}
     </div>
   );
-};
+}
 
 export default Homepage;
