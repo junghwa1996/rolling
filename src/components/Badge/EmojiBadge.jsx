ef75e87906b65933363c0d44eea8ca28d65f7983
양정화 <siltarre@gmail.com>
2024-10-30 12:40:25 +0900

    💄 이모지 뱃지 스타일 디테일하게 수정

--- src/components/Badge/EmojiBadge.jsx
+++ src/components/Badge/EmojiBadge.jsx
@@ -4,12 +4,19 @@
 
 import PropTypes from 'prop-types';
 
-import { EmojiBadgeArea, Emoji, Count } from './EmojiBadge.styles';
+import {
+  EmojiBadgeArea,
+  EmojiContainer,
+  Emoji,
+  Count,
+} from './EmojiBadge.styles';
 
 function EmojiBadge({ emoji, count }) {
   return (
     <EmojiBadgeArea>
-      <Emoji>{emoji}</Emoji>
+      <EmojiContainer>
+        <Emoji>{emoji}</Emoji>
+      </EmojiContainer>
       <Count>{count}</Count>
     </EmojiBadgeArea>
   );
