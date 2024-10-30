14c81e0f4ec8f12c32c98b2d62445f0ad163a427
양정화 <siltarre@gmail.com>
2024-10-31 07:17:19 +0900

    ✏️  전역스타일 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
 import { BrowserRouter } from 'react-router-dom';
-import { styled } from 'styled-components';
 
 import Content from './router/Content';
-
-export const Container = styled.div`
-  width: 100%;
-  max-width: 120rem;
-  margin: 0 auto;
-`;
 
 function App() {
   return (
     <>
-      <Container>
-        <BrowserRouter>
-          <Content />
-        </BrowserRouter>
-      </Container>
+      <BrowserRouter>
+        <Content />
+      </BrowserRouter>
     </>
   );
 }
