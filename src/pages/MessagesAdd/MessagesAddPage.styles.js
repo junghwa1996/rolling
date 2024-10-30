21aaba72c3f506b83db89666a33eec6c720c0d71
Hwichul Shin <shinhwiiron@gmail.com>
2024-10-30 11:07:56 +0900

    💄 px -> rem 수정

--- src/pages/MessagesAdd/MessagesAddPage.styles.js
+++ src/pages/MessagesAdd/MessagesAddPage.styles.js
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
 
 const StyledMessagesAddPage = styled.div`
-  width: 720px;
+  width: 72rem; // NOTE 해당 코드는 임시로 잡아둔 값으로 전체적으로 컴포넌트 조합할 때 수정하겠습니다.
   margin: 0 auto;
 
   button[type='submit'] {
-    margin-top: 62px;
+    margin-top: 6.2rem;
   }
 `;
 
