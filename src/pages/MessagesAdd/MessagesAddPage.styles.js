1eef65c13816ce0c2ac47ccefdfe7ae1558c2fed
정평안 <qwerjpa7@jeongpyeong-an-ui-noteubug.local>
2024-10-31 22:58:21 +0900

    💄 마진 값 더 주어서 드롭다운 내려왔을 때 짤리지 않도록 하기
    마진 값 더 주어서 드롭다운 내려왔을 때 짤리지 않도록 하기

--- src/pages/MessagesAdd/MessagesAddPage.styles.js
+++ src/pages/MessagesAdd/MessagesAddPage.styles.js
@@ -5,7 +5,7 @@ import { tm_color, tm_font } from '../../utils/themeUtils';
 const StyledMessagesAddPage = styled.div`
   width: 72rem; // NOTE 해당 코드는 임시로 잡아둔 값으로 전체적으로 컴포넌트 조합할 때 수정하겠습니다.
   margin: 0 auto;
-  margin-bottom: 8rem; // NOTE 해당 코드는 임시로 잡아둔 값으로 전체적으로 컴포넌트 조합할 때 수정하겠습니다.
+  margin-bottom: 20rem; // NOTE 해당 코드는 임시로 잡아둔 값으로 전체적으로 컴포넌트 조합할 때 수정하겠습니다.
 
   button[type='submit'] {
     margin-top: 6.2rem;
