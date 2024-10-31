85efe211d574db14c34bdaed39ad7a4633de80c5
정평안 <qwerjpa7@jeongpyeong-an-ui-noteubug.local>
2024-10-31 23:34:48 +0900

    🍱 홈페이지에서 사용되는 img 추가
    홈페이지에서 사용되는 img 추가

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,13 @@
 function Homepage() {
   return (
     <div>
-      <h1>홈페이지</h1>
+      <section>
+        <h1>헤더</h1>
+      </section>
+      <section>
+        <h1>part1</h1>
+        <h2>part2</h2>
+      </section>
     </div>
   );
 }
