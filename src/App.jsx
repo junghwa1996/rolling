c5c6f8fcfda3802db32931f7e887b716a1df6af8
tare <59001439+junghwa1996@users.noreply.github.com>
2024-11-01 01:16:09 +0900

    💄 전체 재조정 (현재 작업 영향 없음)

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import { BrowserRouter } from 'react-router-dom';
+// import { ThemeProvider } from 'styled-components';
+// import { useState } from 'react';
 
+import './styles/reset.css';
+import './styles/common.css';
 import Content from './router/Content';
+// import { light, dark } from './styles/theme_mode';
 
 function App() {
+  // const [isDarkMode, setIsDarkMode] = useState(false);
+  // const theme_mode = isDarkMode ? dark : light;
+
+  // const toggleTheme = () => {
+  //   setIsDarkMode((prevMode) => !prevMode);
+  // };
+
   return (
+    // <ThemeProvider theme={theme_mode}>
     <>
-      <BrowserRouter>
-        <Content />
-      </BrowserRouter>
+      {/* <button onClick={toggleTheme}>
+        Switch to {isDarkMode ? 'Light' : 'Dark'} Mode
+      </button> */}
+      <Content />
     </>
+    // </ThemeProvider>
   );
 }
 
