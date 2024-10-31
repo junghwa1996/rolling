af57ae889cba6dc81dc0af0b4474ae87b2301924
swp91 <seongwoo4477@gmail.com>
2024-11-01 07:24:04 +0900

    devietype 아바타그룹관련 생환

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -3,12 +3,15 @@ import PropTypes from 'prop-types';
 
 import Logo from '../../components/Logo/Logo';
 import { HeaderArea, InfoHeader, LogoHeader } from './Header.styles';
+import useDeviceType from '../../hooks/useDeviceType';
 
 Header.propTypes = {
   type: PropTypes.oneOf(['default', 'mobileHidden', 'doubleLine']),
 };
 
 function Header({ type = 'default' }) {
+  const isDeviceType = useDeviceType();
+
   return (
     <HeaderArea $type={type}>
       <div>
@@ -20,7 +23,7 @@ function Header({ type = 'default' }) {
             <div>사용자이름컴포넌트</div>
           </div>
           <div>
-            <div>아바타그룹컴포넌트</div>
+            {isDeviceType === 'pc' && <div>아바타그룹컴포넌트</div>}
             <div>이모지드롭다운컴포넌트</div>
             <div>공유버튼컴포넌트</div>
           </div>
