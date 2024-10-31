2c68470f46dfe10ba61d6f1db86c533ed1c15358
Hwichul Shin <shinhwiiron@gmail.com>
2024-10-31 14:43:04 +0900

    🎨 RadioCard.styles 리펙토링
    이미지 Tab의 경우 GET요청으로 받아 온 데이터를 img src로 뿌려줬는데, 이 부분 컬러 Tab과 동일하게 css background로 동적으로 적용
    
    컬러, 이미지 선택 시 부드럽게 선택되도록 수정

--- src/components/BackgroundSelector/RadioCard.styles.js
+++ src/components/BackgroundSelector/RadioCard.styles.js
@@ -1,4 +1,6 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+import CheckedIcon from '../../assets/icon-checked.svg';
 
 const StyledRadioCard = styled.div`
   display: flex;
@@ -14,28 +16,56 @@ const StyleButton = styled.button`
   padding: 0;
   overflow: hidden;
   position: relative;
-  background: ${({ color }) => color};
+  /* Tab 상태 값에 따라 background 컬러, 이미지 동적 적용 */
+  ${({ activeTab, color, item }) =>
+    activeTab === '컬러'
+      ? css`
+          background: ${color};
+        `
+      : css`
+          background: url(${item}) no-repeat center;
+          background-size: cover;
+        `}
 
-  img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
+  /* 이미지 Tab에서만 이미지 선택 시 투명도 조절 추가 */
+  ${({ activeTab }) =>
+    activeTab === '컬러'
+      ? css``
+      : css`
+          &.selected::after {
+            content: '';
+            background: #fff;
+            width: 100%;
+            height: 100%;
+            position: absolute;
+            top: 0;
+            left: 0;
+            z-index: 1;
+            opacity: 0.3;
+          }
+        `}
 
-  &.selected > img {
-    opacity: 0.3;
-  }
-
-  .icon {
+  &::before {
+    content: '';
+    display: inline-block;
+    width: 4.4rem;
+    height: 4.4rem;
+    background: url(${CheckedIcon});
+    background-size: 100%;
     position: absolute;
     top: 50%;
     left: 50%;
-    transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%) scale(0.5); /* 초기 크기를 작게 설정 */
+    opacity: 0; /* 초기에는 투명 */
+    z-index: 2;
+    transition:
+      transform 0.3s ease,
+      opacity 0.3s ease;
   }
 
-  .icon img {
-    width: 4.4rem;
-    height: 4.4rem;
+  &.selected::before {
+    opacity: 1; /* 선택될 때 서서히 보이게 */
+    transform: translate(-50%, -50%) scale(1); /* 원래 크기로 확대 */
   }
 `;
 
