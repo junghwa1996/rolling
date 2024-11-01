0c14abfd66a4181bcb6b020c79d7af810c7793be
정평안 <qwerjpa7@jeongpyeong-an-ui-noteubug.local>
2024-11-02 03:11:03 +0900

    🔥 홈페이지 정리
    홈페이지 정리

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,7 @@
-import EmojiDropDown from '../../layout/Emoji/EmojiDropDown';
-
 function Homepage() {
   return (
     <div>
       <h1>홈페이지</h1>
-      <EmojiDropDown id={9181} />
     </div>
   );
 }
