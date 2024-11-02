f45a5bb4ad677394b8b799467c38e25720850108
yongcheol <rladydcjf97@naver.com>
2024-11-02 19:30:23 +0900

    📝 추가 수정

--- src/layout/Emoji/SharingSelector.jsx
+++ src/layout/Emoji/SharingSelector.jsx
@@ -1,59 +1,63 @@
 import React, { useState } from 'react';
 
-import { shareToKakao } from './KaKaoShare'; // 카카오 공유 기능 임포트
-import ICON_SHARE from '../../assets/icon-share-24.svg'; // 공유 아이콘 이미지
-import CopyUrl from '../../components/toast/CopyUrl'; // CopyUrl 유틸 함수 임포트
-import {
-  SharingSelectorContainer,
-  Button,
-  Icon,
-  DropdownList,
-  DropdownItem,
-} from './SharingSelector.styles'; // 스타일 임포트
-import { showToast } from '../../components/toast/Toast'; // showToast 함수 임포트
-import { light } from '../../styles/theme'; // 새로운 테마 import
+import { shareToKakao } from './KaKaoShare';
+import { ReactComponent as IconShare } from '../../assets/icon-share-24.svg';
+import CopyUrl from '../../components/toast/CopyUrl';
+import Dropdown from '../../components/TextField/Dropdown';
+import styles from './SharingSelector.module.css'; // 수정된 부분
+import { showToast } from '../../components/toast/Toast';
 
 function SharingSelector() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState({});
 
-  const toggleDropdown = () => {
-    setIsOpen((prev) => !prev); // 드롭다운 열기/닫기
+  const relationshipData = [
+    {
+      value: '카카오톡 공유',
+      label: 'kakaoshare',
+    },
+    {
+      value: 'URL 공유',
+      label: 'urlshare',
+    },
+  ];
+
+  const handleDropdownSelect = (selectedOption) => {
+    setSelectedOption(selectedOption);
+    if (selectedOption.value === '카카오톡 공유') {
+      handleKakaoShareClick();
+    } else if (selectedOption.value === 'URL 공유') {
+      handleUrlShareClick();
+    }
   };
 
   const handleUrlShareClick = () => {
-    const urlToShare = 'https://example.com'; // 공유할 URL
+    const urlToShare = 'https://example.com';
 
-    // CopyUrl 유틸 함수를 사용하여 URL을 클립보드에 복사
     CopyUrl(urlToShare)
       .then(() => {
-        showToast(`"${urlToShare}"가 클립보드에 복사되었습니다.`); // 복사 성공 Toast 메시지
+        showToast(`"${urlToShare}"가 클립보드에 복사되었습니다.`);
       })
       .catch((err) => {
         console.error('URL 복사 실패:', err);
-        showToast('URL 복사에 실패했습니다.'); // 복사 실패 Toast 메시지
+        showToast('URL 복사에 실패했습니다.');
       });
   };
 
   const handleKakaoShareClick = () => {
-    shareToKakao(); // 카카오톡 공유 기능 호출
+    shareToKakao();
   };
 
   return (
-    <SharingSelectorContainer>
-      <Button onClick={toggleDropdown}>
-        <Icon src={ICON_SHARE} alt="Share icon" /> {/* 버튼 안에 이미지 */}
-      </Button>
-      {isOpen && (
-        <DropdownList>
-          <DropdownItem onClick={handleUrlShareClick}>URL 공유</DropdownItem>
-          <DropdownItem
-            onClick={handleKakaoShareClick}
-            style={{ background: light.secondary }}>
-            카카오톡 공유
-          </DropdownItem>
-        </DropdownList>
-      )}
-    </SharingSelectorContainer>
+    <div className={styles.outLinedArea}>
+      <Dropdown
+        hasOptions={{
+          options: relationshipData,
+          selectedOption: selectedOption,
+          onSelect: handleDropdownSelect,
+        }}
+        isIcon={<IconShare />}
+      />
+    </div>
   );
 }
 
