186f143825858bf76ca890af54ca75a8f2f58b2e
yongcheol <rladydcjf97@naver.com>
2024-11-02 22:08:36 +0900

    📝 파일 수정

--- src/components/toast/Toast.jsx
+++ src/components/toast/Toast.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import { toast } from 'react-toastify';
-import { toast } from 'react-toastify';
+import { toast, ToastContainer } from 'react-toastify';
+
 import 'react-toastify/dist/ReactToastify.css';
 import styles from './Toast.module.css'; // 스타일 모듈 가져오기
 import copyUrl from './CopyUrl';
 import ToastMessages from './ToastMessages';
 
-// showToast 함수는 이제 기본적으로 export
-export const showToast = (content) => {
-  copyUrl(content)
 // showToast 함수는 이제 기본적으로 export
 export const showToast = (content) => {
   copyUrl(content)
@@ -19,7 +16,6 @@ export const showToast = (content) => {
             <ToastMessages closeToast={closeToast} />
           </div>
         ),
-
         {
           position: 'bottom-center',
           autoClose: 5000,
@@ -31,13 +27,12 @@ export const showToast = (content) => {
     })
     .catch((error) => {
       console.error('Toast error:', error);
-      console.error('Toast error:', error);
     });
 };
 
 // Toast 컴포넌트에서 handleShowToast 제거
 function Toast() {
-  return <StyledToastContainer className={styles.ToastContainer} />; // StyledToastContainer를 반환
+  return <ToastContainer className={styles.ToastContainer} />; // ToastContainer를 반환
 }
 
 export default Toast;
--- src/components/toast/ToastMessages.jsx
+++ src/components/toast/ToastMessages.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import styles from './Toast.module.css'; // 스타일 모듈 가져오기
 import ICON_CLOSE from '../../assets/icon-close.svg';
 
-function ToastMessages({ closeToast }) {
 function ToastMessages({ closeToast }) {
   return (
     <>
@@ -16,10 +16,6 @@ function ToastMessages({ closeToast }) {
       />
     </>
   );
-}
-
-ToastMessages.propTypes = {
-  closeToast: PropTypes.func.isRequired,
 }
 
 ToastMessages.propTypes = {
