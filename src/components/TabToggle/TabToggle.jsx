866b0546ea8eec319af94a821a50e628b54578bc
홍현빈 <ghdgusqls0101@naver.com>
2024-11-03 03:56:15 +0900

    ♻️  코드 재수정 했습니다

--- src/components/TabToggle/TabToggle.jsx
+++ src/components/TabToggle/TabToggle.jsx
@@ -1,39 +1,39 @@
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
-import styles from './TabToggle.module.css';
-import { SelectedTab } from './TabToggle.styles';
 
-//컴포넌트 정의
+import styles from './TabToggle.module.css';
+import { TabToggleArea, TabButton, SelectedTab } from './TabToggle.styles';
+
 TabToggle.propTypes = {
-  tabs: PropTypes.arrayOf(PropTypes.string), //탭 이름 배열
-  onClick: PropTypes.func, //탭 클릭 시 호출되는 함수
+  tabs: PropTypes.arrayOf(PropTypes.string),
+  onClick: PropTypes.func,
 };
 
-//현재 선택된 탭 상태 관리
 function TabToggle({ tabs = ['컬러', '이미지'], onClick = () => {} }) {
   const [currentTab, setCurrentTab] = useState(tabs[0]);
 
-  //탭 클릭 핸들러
   const handleTabClick = (tab) => {
-    setCurrentTab(tab); //선택된 탭 업데이트
-    onClick(tab); //클릭 이벤트 발생
+    setCurrentTab(tab);
+    onClick(tab);
   };
 
   return (
-    <div
-      className={styles.tabToggleArea}
-      style={{ width: `${12 * tabs.length}rem` }}>
-      <SelectedTab selectedIndex={tabs.indexOf(currentTab)} />
+    <TabToggleArea className={styles.tabToggleArea} $tabLength={tabs.length}>
+      <SelectedTab
+        className={styles.selectedTab}
+        $selectedIndex={tabs.indexOf(currentTab)}
+      />
       {tabs.map((tab) => (
-        <button
+        <TabButton
           key={tab}
-          className={`${styles.tabButton} ${currentTab === tab ? styles.tabButtonSelected : ''}`}
+          className={styles.tabButton}
+          $isSelected={currentTab === tab}
           onClick={() => handleTabClick(tab)}
           type="button">
           {tab}
-        </button>
+        </TabButton>
       ))}
-    </div>
+    </TabToggleArea>
   );
 }
 
--- src/components/TabToggle/TabToggle.styles.js
+++ src/components/TabToggle/TabToggle.styles.js
@@ -2,16 +2,27 @@ import styled from 'styled-components';
 
 import { font } from '../../styles/fontStyles';
 import { color } from '../../styles/colorStyles';
+import styles from './TabToggle.module.css';
 
-/* 선택된 탭을 나타내는 스타일 컴포넌트 */
+export const TabToggleArea = styled.div`
+  width: ${({ $tabLength }) => `${12 * $tabLength}rem`};
+  &.tabToggleArea {
+    composes: tabToggleArea;
+  }
+`;
+
+export const TabButton = styled.button`
+  ${({ $isSelected }) => ($isSelected ? font['16b'] : font[16])};
+  color: ${({ $isSelected, theme }) =>
+    $isSelected ? color.purple[600] : theme.text};
+  &.tabButton {
+    composes: tabButton;
+  }
+`;
+
 export const SelectedTab = styled.div`
-  position: absolute;
-  top: 0;
-  left: ${({ selectedIndex }) => `${12 * selectedIndex}rem`};
-  width: 12rem;
-  height: 100%;
-  border: 2px solid ${({ theme }) => theme.purple[600]};
-  border-radius: 6px;
-  background: #fff;
-  transition: left 0.3s ease;
+  left: ${({ $selectedIndex }) => `${12 * $selectedIndex}rem`};
+  &.selectedTab {
+    composes: selectedTab;
+  }
 `;
