6cf51a534a413559b947a4c3a7330644c3ddc6f2
Hwichul Shin <shinhwiiron@gmail.com>
2024-11-02 12:58:02 +0900

    ✨ 뒤로가기 컨펌 커스텀 훅 기능 추가

---
+++ src/hooks/useBeforeUnload.js
@@ -0,0 +1,19 @@
+import { useEffect } from 'react';
+
+function useBeforeUnload() {
+  const preventRefresh = (event) => {
+    event.preventDefault();
+    event.returnValue = '';
+  };
+
+  useEffect(() => {
+    // beforeunload : 브라우저 제공 이벤트이며, 사용자가 페이지를 떠나기 전에 발생하며, 주로 페이지를 떠날 때 확인 메시지를 보여줌.
+    window.addEventListener('beforeunload', preventRefresh);
+
+    return () => {
+      window.removeEventListener('beforeunload', preventRefresh);
+    };
+  }, []);
+}
+
+export default useBeforeUnload;
