58e6377afdfcb0f90effb51cbd67e60176fbe9e5
tare <59001439+junghwa1996@users.noreply.github.com>
2024-11-03 18:08:21 +0900

    💄 스타일수정

--- src/components/CardComponents/CardAdd/CardAdd.styles.js
+++ src/components/CardComponents/CardAdd/CardAdd.styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-const CardContainer = styled.a`
+export const CardContainer = styled.a`
   display: flex;
   align-items: center;
   justify-content: center;
