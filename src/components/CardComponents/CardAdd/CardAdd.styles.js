557175cca46c0add600850c7e40169280aa4a7fe
tare <59001439+junghwa1996@users.noreply.github.com>
2024-11-03 18:30:13 +0900

    ✨ 새롭게추가

---
+++ src/components/CardComponents/CardAdd/CardAdd.styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+import { cardBaseStyle } from '../../../styles/common/Common.styles';
+
+export const CardContainer = styled.a`
+  ${cardBaseStyle}
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
