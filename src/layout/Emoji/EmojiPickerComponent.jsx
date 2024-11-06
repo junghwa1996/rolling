f3dd38fa656e4af177df05cb95a5f5b14fac4eac
yongcheol <rladydcjf97@naver.com>
2024-11-06 20:17:14 +0900

    📝 확인

--- src/layout/Emoji/EmojiPickerComponent.jsx
+++ src/layout/Emoji/EmojiPickerComponent.jsx
@@ -14,7 +14,7 @@ EmojiPickerComponent.propTypes = {
 
 function EmojiPickerComponent({ onEmojiAdd, isLoading }) {
   const [showPicker, setShowPicker] = useState(false);
-  const pickerRef = useRef(null);  // Picker 외부 클릭을 감지할 ref
+  const pickerRef = useRef(null); // Picker 외부 클릭을 감지할 ref
   const isDevice = useDeviceType();
   const isMo = isDevice === 'mobile';
 
@@ -26,9 +26,9 @@ function EmojiPickerComponent({ onEmojiAdd, isLoading }) {
       }
     };
 
-    document.addEventListener('mousedown', handleClickOutside);  // 마우스 클릭 시 외부 클릭 감지
+    document.addEventListener('mousedown', handleClickOutside); // 마우스 클릭 시 외부 클릭 감지
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);  // 컴포넌트 언마운트 시 이벤트 리스너 정리
+      document.removeEventListener('mousedown', handleClickOutside); // 컴포넌트 언마운트 시 이벤트 리스너 정리
     };
   }, []);
 
