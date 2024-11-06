bf05a15d00d372b334f81cef6220b9d40716b191
tare <59001439+junghwa1996@users.noreply.github.com>
2024-11-07 08:41:13 +0900

    🐛

--- src/layout/Emoji/EmojiPickerComponent.jsx
+++ src/layout/Emoji/EmojiPickerComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import Picker from 'emoji-picker-react';
 import PropTypes from 'prop-types';
 
